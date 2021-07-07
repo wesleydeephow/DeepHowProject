570617f1519e1148ffb0550455b1b0941a826101
陳曉明 <wesley.chen@deephow.com>
2021-07-07 15:53:00 +0800

    0707 1. insert Signin.py 2. edit GroupTests.py

--- Web/GroupTests.py
+++ Web/GroupTests.py
@@ -1,12 +1,13 @@
 import unittest
 from HellowTest import HellowTestCase
+from Signin import SigninCase
 
 # get all tests from SearchProductTest and HomePageTest class
 HellowTest = unittest.TestLoader().loadTestsFromTestCase(HellowTestCase)
-
+Signin = unittest.TestLoader().loadTestsFromTestCase(SigninCase)
 
 # create a test suite combining search_test and home_page_test
-GroupTests = unittest.TestSuite([HellowTest])
+GroupTests = unittest.TestSuite([HellowTest, Signin])
 # run the suite
 unittest.TextTestRunner(verbosity=2).run(GroupTests)
 
---
+++ Web/Signin.py
@@ -0,0 +1,56 @@
+import unittest
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+class SigninCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()    #視窗最大化
+        self.driver.implicitly_wait(600)  #隱式等待60秒
+        self.driver.get("https://dev.deephow.net/")
+
+    def test_singin(self):
+        driver = self.driver
+        time.sleep(2)
+        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div[2]/div/div/div[1]/div[2]/div/div[1]/form/div/div/div[1]/div/input'))) #顯示等待
+        driver.find_element_by_xpath('//*[@id="app"]/div[2]/div/div/div[1]/div[2]/div/div[1]/form/div/div/div[1]/div/input').send_keys("wesley.chen+root@deephow.com")
+        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, "v-btn__content")))
+        driver.find_element_by_class_name("v-btn__content").click()
+        acct = driver.find_element_by_class_name("log-in-title").text
+        print(acct)
+        self.assertEqual(acct, "你好, Wesley.Chen+root!", "acct Error") #驗證(expected預期在前，actual實際在後)
+        time.sleep(2)
+        driver.find_element_by_class_name("back-btn").click()
+        time.sleep(2)
+        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, "v-btn__content")))
+        driver.find_element_by_class_name("v-btn__content").click()
+        acct = driver.find_element_by_class_name("log-in-title").text
+        print(acct)
+        self.assertEqual(acct, "你好, Wesley.Chen+root!", "acct Error") #驗證(expected預期在前，actual實際在後)
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="app"]/div[2]/div/div/div[1]/div[2]/div/form/div/div/div[1]/div[1]/input').send_keys("Pa$$w0rd")
+        time.sleep(2)
+        driver.find_element_by_class_name("v-btn__content").click()
+        time.sleep(2)
+        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, "v-menu__activator")))
+        driver.find_element_by_class_name("v-menu__activator").click()
+        time.sleep(2)
+        name = driver.find_element_by_class_name("v-list__tile__title").text
+        email = driver.find_element_by_class_name("v-list__tile__sub-title").text
+        print(name)
+        print(email)
+        self.assertEqual(name, "R管理者", "Name Error") #驗證(expected預期在前，actual實際在後)
+        self.assertEqual(email, "wesley.chen+root@deephow.com", "email Error") #驗證(expected預期在前，actual實際在後)
+        time.sleep(2)
+        driver.find_element_by_class_name("signout-btn").click()
+        time.sleep(2)
+
+    def tearDown(self):
+        self.driver.close()
+        self.driver.quit()
+
+if __name__ == '__main__':
+     unittest.main(verbosity=2)
