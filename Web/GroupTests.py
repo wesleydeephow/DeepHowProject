dc498372364ac4cf808b22208cb0a3e5e0490019
陳曉明 <wesley.chen@deephow.com>
2021-07-26 16:17:40 +0800

    0726v2 1. update GroupTests.py

--- Web/GroupTests.py
+++ Web/GroupTests.py
@@ -2,14 +2,16 @@ import unittest
 from Signin import SigninCase
 from Business import BusinessCase
 from Workspaces import WorkspacesCase
+from InviteUser import InvitesUserCase
 
 # get all tests from SearchProductTest and HomePageTest class
 Signin = unittest.TestLoader().loadTestsFromTestCase(SigninCase)
 Business = unittest.TestLoader().loadTestsFromTestCase(BusinessCase)
 Workspaces = unittest.TestLoader().loadTestsFromTestCase(WorkspacesCase)
+InviteUser = unittest.TestLoader().loadTestsFromTestCase(InvitesUserCase)
 
 # create a test suite combining search_test and home_page_test
-GroupTests = unittest.TestSuite([Signin, Business, Workspaces])
+GroupTests = unittest.TestSuite([Signin, Business, Workspaces, InviteUser])
 
 # run the suite
 unittest.TextTestRunner(verbosity=2).run(GroupTests)
