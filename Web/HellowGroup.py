ca0f09e16b626498080efa5f926ed7f20122efe3
陳曉明 <wesley.chen@deephow.com>
2021-08-04 12:03:25 +0800

    0804 1. edit HellowGroup.py 2. add HellowWhile.py

--- Web/HellowGroup.py
+++ Web/HellowGroup.py
@@ -1,12 +1,14 @@
 import unittest
 from HellowTest import HellowTestCase
+from HellowWhile import HellowWhileCase
 
 # get all tests from SearchProductTest and HomePageTest class
 HellowTest = unittest.TestLoader().loadTestsFromTestCase(HellowTestCase)
+HellowWhile = unittest.TestLoader().loadTestsFromTestCase(HellowWhileCase)
 
 
 # create a test suite combining search_test and home_page_test
-HellowGroup = unittest.TestSuite([HellowTest])
+HellowGroup = unittest.TestSuite([HellowTest, HellowWhile])
 
 # run the suite
 unittest.TextTestRunner(verbosity=2).run(HellowGroup)
---
+++ Web/HellowWhile.py
@@ -0,0 +1,51 @@
+import unittest
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+class HellowWhileCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()    #視窗最大化
+        self.driver.implicitly_wait(600)  #隱式等待60秒
+        self.driver.get("https://dev.deephow.net/")
+        time.sleep(2)
+        WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div[2]/div/div/div[1]/div[2]/div/div[1]/form/div/div/div[1]/div/input'))) #顯示等待
+        self.driver.find_element_by_xpath('//*[@id="app"]/div[2]/div/div/div[1]/div[2]/div/div[1]/form/div/div/div[1]/div/input').send_keys("wesley.chen+root@deephow.com")
+        WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, "v-btn__content")))
+        self.driver.find_element_by_class_name("v-btn__content").click()
+        time.sleep(2)
+        WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div[2]/div/div/div[1]/div[2]/div/form/div/div/div[1]/div[1]/input'))) #顯示等待
+        self.driver.find_element_by_xpath('//*[@id="app"]/div[2]/div/div/div[1]/div[2]/div/form/div/div/div[1]/div[1]/input').send_keys("Pa$$w0rd")
+        WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, "v-btn__content")))
+        self.driver.find_element_by_class_name("v-btn__content").click()
+
+    def test_hellowwhile(self):
+        driver = self.driver
+        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, "tool-bar-icon")))
+        driver.find_element_by_class_name("tool-bar-icon").click()
+        driver.find_element_by_link_text("管理").click()
+        time.sleep(2)
+
+        i = 1
+        while i <= 3:
+          WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, "tool-bar-icon")))
+          driver.find_element_by_class_name("tool-bar-icon").click()
+          driver.find_element_by_link_text("播放器").click()
+          time.sleep(2)
+
+          WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, "tool-bar-icon")))
+          driver.find_element_by_class_name("tool-bar-icon").click()
+          driver.find_element_by_link_text("编辑器").click()
+          time.sleep(2)
+          i = i + 1
+          print(i)
+
+    def tearDown(self):
+        self.driver.close()
+        self.driver.quit()
+
+if __name__ == '__main__':
+     unittest.main(verbosity=2)
