fc5ae73f886318efe7923c13d6b81af457d34e08
陳曉明 <wesley.chen@deephow.com>
2021-10-18 09:56:47 +0800

    1018

--- Web/HellowGroup.py
+++ Web/HellowGroup.py
@@ -1,16 +1,17 @@
 import unittest
-from HellowWhile import HellowWhileCase
+#from HellowWhile import HellowWhileCase
 from HellowTest import HellowTestCase
 
 
 # get all tests from SearchProductTest and HomePageTest class
-HellowWhile = unittest.TestLoader().loadTestsFromTestCase(HellowWhileCase)
+#HellowWhile = unittest.TestLoader().loadTestsFromTestCase(HellowWhileCase)
 HellowTest = unittest.TestLoader().loadTestsFromTestCase(HellowTestCase)
 
 
 
 # create a test suite combining search_test and home_page_test
-HellowGroup = unittest.TestSuite([HellowWhile, HellowTest])
+#HellowGroup = unittest.TestSuite([HellowWhile, HellowTest])
+HellowGroup = unittest.TestSuite([HellowTest])
 
 # run the suite
 unittest.TextTestRunner(verbosity=2).run(HellowGroup)
